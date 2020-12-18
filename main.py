8376855a081955663b65195a95e6f5c816a0457f
mexalon <nabarysh@gmail.com>
2020-12-18 17:41:17 +0300

    principle indexing done

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 # project about students
 
 class Human:
-    def __init__(self, name, surename):
+    def __init__(self, name, surename = 'Smith'):
         self.name = name
         self.surname = surename
         self.__role = []
 
+# директор тоже человек, он назначает на роли разные классы и ведёт журнал учёта
 
 class Principle(Human):
     def __init__(self, name, surename):
         super().__init__(name, surename)
         self._Human__role.append('Principle')
-        self.index = [{self: self._Human__role}]
-#
+        self.index = {self: self._Human__role}
+
     def recruit_as(self, other, some_role):
         pass
         other._Human__role.append(some_role)
-        self.index.append({other: other._Human__role})
+        self.index[other] = other._Human__role
+
 
 class Student(Human):
     pass
@@ -32,15 +34,19 @@ class Reviewer(Human):
 
 aaa = Principle('Pet', 'Jhons')
 bbb = Lector('Baba', 'Gump')
+ccc = Student('Pepe')
+
+
 aaa.recruit_as(bbb, 'shrimplover')
 aaa.recruit_as(bbb, 'janitor')
+aaa.recruit_as(ccc, 'stupid student')
+
 
 #print(aaa._Human__role)
-for person in aaa.index:
-    for persons_class in person.keys():
-        print(f'имя: {persons_class.name} \n'
-              f'фамилия: {persons_class.surname} \n'
-              f'роли:  {person[persons_class]}')
+for person in aaa.index.keys():
+    print(f'имя: {person.name} \n'
+          f'фамилия: {person.surname} \n'
+          f'роли:  {aaa.index[person]}')
 
 # test = {'names': 'tesssssting'}
 # for entry in test.keys():
